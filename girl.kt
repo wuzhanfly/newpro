fa5ee201434c8de68543606149739e40139d7cd4
wuzhanfly <wuzhanflu@gmail.com>
2018-05-11 16:26:13 +0800

    mary的第一次修改

---
+++ girl.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val person= Person1(name="kk",age=20,phone = "12345568")
+
+
+
+}
+open class Person(var name:String?=null,var age:Int？ = null,var phone:String? = null ){
+	fun sayhello(person Person){
+		println.（"$name ,你好"）
+	}
+}
